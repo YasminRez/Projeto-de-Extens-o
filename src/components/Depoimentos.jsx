3cd57c44fe194889deceacb68804edb0ee99a72f
Renata Aguiar <you@example.com>
2025-06-21 22:48:35 -0300

    Finalização dos demais componentes

--- src/components/Depoimentos.jsx
+++ src/components/Depoimentos.jsx
@@ -1,63 +1,54 @@
 function Depoimentos() {
-    return (
-        <div id="depoimentos">
-            <h1>Depoimentos</h1>
-            <div class="carrossel">
-                <div id="caixinha-depoimentos"> 
-                    <div id="imagem-depoimento">
-                        {/* <img src="caminho/da-imagem.jpg" alt="Descrição da imagem"> */}
-                    </div>
-                    <p> ★★★★★ </p>
-                    <h3>Cinthy Luiza</h3>
-                    <p> Depois da pandemia, eu passei a ter um medo tremendo de ficar doente. Isso me fez reduzir muito minhas idas a academia. 
-                        Esse ano e com a sua consultoria, retomei em 100% meus treinos.
-                        Cheguei aos 56 kg, pra quem não saía dos 50 kg.
-                        Tenho energia, ânimo, vigor. Consequentemente todas a minha outras atividades, principalmente as laborais, tem rendido. 
-                        Treinar o corpo, alivia a mente. Seus stories me motivam muito, musa! 
-                        Tu é foda e espero traçar muitos outros objetivos pra esse corpinho, com a sua ajuda!! Hahaha boraaaaaa 
-                    </p>
-                </div>
-                
-                <div id="caixinha-depoimentos"> 
-                    <div id="imagem-depoimento">
-                        {/* <img src="caminho/da-imagem.jpg" alt="Descrição da imagem"> */}
-                    </div>
-                    <p> ★★★★★ </p>
-                    <h3>Vivi Ricci</h3>
-                    <p> Pois há um ano e meio eu era outra pessoa, sem força! 
-                        Hoje, depois dos seus treinos, viajo, caminho e vou em shows sem notar a idade que tenho! 
-                        Como seus treinos fazem a diferença!!! Obrigada!
-                    </p>
-                </div>
+  return (
+    <div id="depoimentos">
+      <div className="container-titulo">
+        <h1> Depoimentos de alguns clientes </h1>
+      </div>
 
-                <div id="caixinha-depoimentos"> 
-                    <div id="imagem-depoimento">
-                        {/* <img src="caminho/da-imagem.jpg" alt="Descrição da imagem"> */}
-                    </div>
-                    <p> ★★★★★ </p>
-                    <h3>Priscila Bonduki </h3>
-                    <p> GRAÇAS A VOCÊ QUE ESTÁ QUASE TODOS OS DIAS ME "MATANDO" 
-                        NA AULA PARA NÓS DEIXAR VIVA E ATIVA POR MUITOS ANOS!!! 
-                        Obrigada por tudo!!
-                    </p>
-                </div>
-    
-                <div id="caixinha-depoimentos"> 
-                    <div id="imagem-depoimento">
-                        {/* <img src="caminho/da-imagem.jpg" alt="Descrição da imagem"> */}
-                    </div>
-                    <p> ★★★★★ </p>
-                    <h3>Yasmin Rezende</h3>
-                    <p> No meu caso não conseguia nem praticar esportes, não conseguia correr e nem me movimentar direito. 
-                        Não aguentava nem um minuto de corrida intensa mas com treino agora consigo participar de corridas de 5/10km tranquilamente e ainda vencer. 
-                        Em questão de sair de casa nem saia pq não tinha autoestima ou pq muitas vezes a pessoa nem chamavam por achar q eu n ia conseguir ir, 
-                        porém agora vou para todo lugar show, jogos e entre outras coisas. 
-                        Então fez bastante diferença perder mais de 30kg. 
-                    </p>
-                </div>
-            </div>
+      <div className="container-depoimentos">
+        <div className="caixinha-depoimento">
+          <h3>Cinthy Luiza</h3>
+          <p>
+            Depois da pandemia, eu passei a ter um medo tremendo de ficar doente. Isso me fez reduzir muito minhas idas à academia. 
+            Esse ano e com a sua consultoria, retomei em 100% meus treinos.
+            Cheguei aos 56 kg, pra quem não saía dos 50 kg.
+            Tenho energia, ânimo, vigor. Consequentemente todas a minha outras atividades, principalmente as laborais, têm rendido. 
+            Treinar o corpo alivia a mente. Seus stories me motivam muito, musa! 
+            Tu é foda e espero traçar muitos outros objetivos pra esse corpinho, com a sua ajuda!! Hahaha boraaaaaa.
+          </p>
         </div>
-    );
+
+        <div className="caixinha-depoimento">
+          <h3>Vivi Ricci</h3>
+          <p>
+            Pois há um ano e meio eu era outra pessoa, sem força! 
+            Hoje, depois dos seus treinos, viajo, caminho e vou em shows sem notar a idade que tenho! 
+            Como seus treinos fazem a diferença!!! Obrigada!
+          </p>
+        </div>
+
+        <div className="caixinha-depoimento">
+          <h3>Priscila Bonduki</h3>
+          <p>
+            Graças a você que está quase todos os dias me "matando" 
+            na aula para nos deixar vivas e ativas por muitos anos!!! 
+            Obrigada por tudo!!
+          </p>
+        </div>
+
+        <div className="caixinha-depoimento">
+          <h3>Yasmin Rezende</h3>
+          <p>
+            No meu caso não conseguia nem praticar esportes, não conseguia correr e nem me movimentar direito. 
+            Não aguentava nem um minuto de corrida intensa, mas com treino agora consigo participar de corridas de 5/10km tranquilamente e ainda vencer. 
+            Em questão de sair de casa, nem saia porque não tinha autoestima ou porque muitas vezes as pessoas nem chamavam por achar que eu não ia conseguir ir. 
+            Porém agora vou para todo lugar: shows, jogos e entre outras coisas. 
+            Então fez bastante diferença perder mais de 30kg.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Depoimentos
+export default Depoimentos;
